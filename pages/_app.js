3a3b26e905db479bd7773024d26796fa03cfa353
valoudefou <valerian.karsenty@gmail.com>
2024-07-10 14:18:32 +0100

    UPGRADE

--- pages/_app.js
+++ pages/_app.js
@@ -1,99 +1,123 @@
 import '../styles/globals.css'
 import Head from 'next/head'
-import { Flagship, FlagshipProvider, HitType, useFlagship, useFsFlag } from "@flagship.io/react-sdk"
-import React from "react"
+import { Flagship, FlagshipProvider, useFsFlag } from "@flagship.io/react-sdk"
 import App from "next/app"
-import Footer from '../components/Footer'
 import { v4 as uuidv4 } from 'uuid'
-import { useEffect } from 'react'
+import { createContext, useState, useEffect } from 'react'
 
+export const AppContext = createContext()
 
 function MyApp({ Component, pageProps, initialFlagsData, initialVisitorData }) {
-// AB Tasty anti flicker
-useEffect(() => {
-if (typeof window !== 'undefined') {
-        const antiFlicker = document.querySelector('#ab-tasty-anti-flicker')
-        if (antiFlicker && window.ABTasty !== 'undefined') {
-            window.addEventListener('abtasty_executedCampaign', (event) => {
-                // console.log(event.detail)
-                function antiFlicker() {
-                    const antiFlicker = document.querySelector('#ab-tasty-anti-flicker')  
-                    antiFlicker.style.visibility = 'hidden'
-                }
-                setTimeout(antiFlicker, 500)
-            })
+    const [isShown, setIsShown] = useState(false)
+
+    useEffect(() => {
+        localStorage.setItem('FS_VISITOR', initialVisitorData.id) // BYOID in localStorage
+        document.cookie = 'FS_VISITOR=' + initialVisitorData.id // BYOID in a cookie
+        
+        if (typeof window !== 'undefined') {
+            const antiFlicker = document.querySelector('#ab-tasty-anti-flicker')
+            
+            if (antiFlicker && window.ABTasty !== 'undefined') {
+                window.addEventListener('abtasty_executedCampaign', () => {
+                    function antiFlicker() {
+                        const antiFlicker = document.querySelector('#ab-tasty-anti-flicker')  
+                        antiFlicker.style.visibility = 'hidden'
+                    }
+                    setTimeout(antiFlicker, 500)
+                })
+            } 
         } 
-    } 
 
-    // anti flicker when AB Tasty is not defined
-    window.addEventListener('load', () => {
-        const antiFlicker = document.querySelector('#ab-tasty-anti-flicker') 
-        if (antiFlicker) {
-            antiFlicker.style.visibility = 'hidden'
-        }
-    })
-}, [])
+        // Anti-flicker when AB Tasty is not defined
+        window.addEventListener('load', () => {
+            const antiFlicker = document.querySelector('#ab-tasty-anti-flicker') 
 
-//get flag 
-const fs = useFlagship()
-const flagIndustry = useFsFlag("flagIndustry", "Product")
-return (
-<>
-<FlagshipProvider
-onVisitorExposed={({ exposedVisitor, fromFlag }) => {
-    window.abtasty.send("segment", {
-        s: {
-          userId: exposedVisitor.id,
-          flagInfo: fromFlag.metadata
-        }
-    })
-}}
-envId={"cimqt0uqvn36gba8e4b0"}
-apiKey={"fzGFlVlcAWBjKJAAAXcPfSQcAHHQkqJekDprRisp"}
-visitorData={initialVisitorData}
-initialFlagsData={initialFlagsData || {}}>
+            if (antiFlicker) {
+                antiFlicker.style.visibility = 'hidden'
+            }
+        })
+    }, [])
 
-<Head />
-<title>{'The ' + flagIndustry.getValue() + ' House'}</title>
-<Component {...pageProps} />
-<Footer />
+    // Get flag 
+    const flagIndustry = useFsFlag("flagIndustry", "Product")
 
-</FlagshipProvider>
-</>
-)
+    return (
+        <>
+            <AppContext.Provider value={[isShown, setIsShown]}>
+                <FlagshipProvider
+                    envId={process.env.NEXT_PUBLIC_FS_ENV}
+                    apiKey={process.env.NEXT_PUBLIC_FS_KEY}
+                    visitorData={initialVisitorData}
+                    initialFlagsData={initialFlagsData || {}}
+                    onVisitorExposed={({ exposedVisitor, fromFlag }) => 
+                        dataLayer.push({
+                            'event': 'abtasty_flag',
+                            "campaignId": fromFlag.metadata.campaignId,
+                            "campaignType": fromFlag.metadata.campaignType,
+                            "isReference": fromFlag.metadata.isReference,
+                            "key": fromFlag.key,
+                            "slug": fromFlag.metadata.slug,
+                            "val": fromFlag.value,
+                            "variationId": fromFlag.metadata.variationId,
+                            "variationName": fromFlag.metadata.variationName,
+                        })
+                    }
+                >
+                    <Head/>
+                    <title>{'The ' + flagIndustry.getValue() + ' House'}</title>
+                    <Component {...pageProps} />
+                </FlagshipProvider>
+            </AppContext.Provider>
+        </>
+    )
 }
+
 MyApp.getInitialProps = async (appContext) => {
-const appProps = await App.getInitialProps(appContext)
+    const appProps = await App.getInitialProps(appContext)
 
-//Start the Flagship SDK
-const flagship = Flagship.start("cimqt0uqvn36gba8e4b0", "fzGFlVlcAWBjKJAAAXcPfSQcAHHQkqJekDprRisp", {
-fetchNow: false,
-})
-const initialVisitorData = {
-id: uuidv4(),
-context: {
-organisation: "whatever",
-segment: "jewelry",
-login: 'true',
-system: "ios",
-regionId: 3,
-},
-}
-// Create a new visitor
-const visitor = flagship?.newVisitor({
-visitorId: initialVisitorData.id,
-context: initialVisitorData.context,
-})
+    // Start the Flagship SDK
+    const flagship = Flagship.start(process.env.NEXT_PUBLIC_FS_ENV, process.env.NEXT_PUBLIC_FS_KEY, {
+        fetchNow: false,
+    })
 
-//Fetch flags
-await visitor?.fetchFlags()
+    const initialVisitorData = {
+        // id: '3c970578-679d-49a6-81b9-cdad6960a63b',
+        id: uuidv4(),
+            context: {
+            organisation: "whatever",
+            device: 'mobile',
+            store: 'US',
+            subscription: 'true',
+            segment: 'cosmetic',
+            store: '1',
+            profile: 'something',
+            positioning: 'terrace',
+            member: 'true',
+            beta: 'test',
+            product: '20948209482098490284',
+            login: 'true',
+            book: 'test',
+            sku: '92842942398',
+            ios: "true",
+            regionId: 3,
+            ios: '0987942782'
+        },
+    }
+    // Create a new visitor
+    const visitor = flagship?.newVisitor({
+        visitorId: initialVisitorData.id,
+        context: initialVisitorData.context,
+    })
 
-// Pass data to the page via props
-return {
-...appProps,
-initialFlagsData: visitor?.getFlagsDataArray(),
-initialVisitorData,
-}
+    // Fetch flags
+    await visitor?.fetchFlags()
+
+    // Pass data to the page via props
+    return {
+        ...appProps,
+        initialFlagsData: visitor?.getFlagsDataArray(),
+        initialVisitorData,
+    }
 }
 
 export default MyApp
