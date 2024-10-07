92a935c6b212a595e01a0ba042c9964ffe08a64c
valoudefou <valerian.karsenty@gmail.com>
2024-10-07 18:25:19 +0100

    TEST

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,20 @@
 import Image from 'next/image'
-import { HitType, useFsFlag } from "@flagship.io/react-sdk"
+import { HitType, useFsFlag, useFlagship } from "@flagship.io/react-sdk"
 import Link from 'next/link'
 import Navbar from '../components/Navbar'
 
 export default function Header() {
 const fs = useFsFlag()
+const { getFlag } = useFlagship()
 
 // Get flag 
-const flagImageSrcVal = useFsFlag("flagImageSrc")
+const flagImageSrcVal = getFlag("flagImageSrc")
 const flagImageSrc = flagImageSrcVal.getValue("/jewelry.jpg")
-const flagBtnText = useFsFlag("flagBtnText", "Shop")
-const flagIndustry = useFsFlag("flagIndustry", "Product")
-const flagBackgroundColor = useFsFlag("flagBackgroundColor", "black")
+const flagBtnTextVal = getFlag("flagBtnText", "Shop")
+const flagBtnText = flagBtnTextVal.getValue()
+const flagIndustryVal = getFlag("flagIndustry", "Product")
+const flagIndustry = flagIndustryVal.getValue()
+const flagBackgroundColor = getFlag("flagBackgroundColor", "black")
 
   return (
     <header className="relative">
@@ -25,17 +28,17 @@ const flagBackgroundColor = useFsFlag("flagBackgroundColor", "black")
                 layout='fill'
                 className="h-full w-full object-cover"
                 src={flagImageSrc}
-                alt={flagIndustry.getValue()}
+                alt={flagIndustry}
               />
               <div className="absolute inset-0" />
             </div>
             <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
             <p className="mx-auto max-w-xl text-center text-xl font-semibold uppercase tracking-wide text-white">
-            {'The ' + flagIndustry.getValue() + ' House'}
+            {'The ' + flagIndustry + ' House'}
             </p>
             <h1 className="mt-1 text-center font-semibold text-gray-900 text-4xl sm:text-5xl">
             <span className="block text-white">Life is better with</span>
-            <span className="block" style={{color: flagBackgroundColor.getValue()}}>{flagIndustry.getValue()}</span>
+            <span className="block" style={{color: flagBackgroundColor.getValue()}}>{flagIndustry}</span>
             </h1>
             <div className="mt-10 flex justify-center">
               <Link href="/products">
@@ -51,7 +54,7 @@ const flagBackgroundColor = useFsFlag("flagBackgroundColor", "black")
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1" stroke="currentColor" className="w-6 h-6 py-1">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"/>
                   </svg>
-                  {flagBtnText.getValue()}
+                  {flagBtnText}
                 </button>
               </Link>
             </div>
